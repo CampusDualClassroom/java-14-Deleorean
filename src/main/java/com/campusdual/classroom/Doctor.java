66eb693c954f6aace8d688855474afcf700f7e28
Edugonzalez <eduu.gonzalez@gmail.com>
2024-11-14 20:56:13 +0100

    Ejercicio 14 resuelto

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -2,10 +2,15 @@ package com.campusdual.classroom;
 
 public class Doctor extends Person {
 
-	protected String specialization;
+    protected String specialization;
 
-	public Doctor(String name, String surname, String specialization) {
-		super(name, surname);
-		this.specialization = specialization;
-	}
+    public Doctor(String name, String surname, String specialization) {
+        super(name, surname);
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void getDetails() {
+        System.out.println("Este/a médico/a se llama " + name + " " + surname + " y su especialización es: " + specialization);
+    }
 }
